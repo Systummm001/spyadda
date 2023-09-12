835e5ec7cadc499081d2e8931bc0fabb675e7dfc
Spy <119489004+amthespy@users.noreply.github.com>
2023-09-12 17:52:57 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -1,27 +1,32 @@
+#Github.com/Vasusen-code
+
 from pyrogram import Client
+
+from telethon.sessions import StringSession
 from telethon.sync import TelegramClient
-import logging
-import sys
+
+from decouple import config
+import logging, time, sys
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.WARNING)
 
 # variables
-API_ID = 24490919
-API_HASH = "d1b3b15126c47dd4cb491553ee1db910"
-BOT_TOKEN = "6187925993:AAEA54GeuVY5Ju9tOZvhgwSNBQr6f4gc-4k"
-SESSION = "BQC6JkIkHLfGZpi8gnxgY_IyjkTBiKXNqRuVQyXG0bV_LRh_HFgj91EEkMU0sGlLC5T7N9NVzlfCfMG8025sqEwbJ8GoDLUV0eAegMvVFzmjNReePmgnoNwfRpeMFbMv4V26a-AZIGRUJsQr2S8VAmFoBY-zxvkv8u6Hs4a5p-P_PXyutZDPSNn-bfrrqVxpGCi8QpqQjPaCfo-xnDWExz4TWFIJ6X6mZ8s-kRCNvmrV0pGvLFojD4tqUAA54A2qI0dv04GlcPumYPiNCGQPCYQdFY4LiRHKPkjBHh0xWsRv7SN2pHrRxd1-_3o1QbZPYjUzyH_PavVAOAAAAAXTGvwYA"
-FORCESUB = "batchingupdate"
-AUTH = 6302251074
+API_ID = config("API_ID", default=None, cast=int)
+API_HASH = config("API_HASH", default=None)
+BOT_TOKEN = config("BOT_TOKEN", default=None)
+SESSION = config("SESSION", default=None)
+FORCESUB = config("FORCESUB", default=None)
+AUTH = config("AUTH", default=None, cast=int)
 
-bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
+bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
-userbot = userbot = Client("saverestricted", session_string=SESSION, api_hash=API_HASH, api_id=API_ID)
+userbot = Client("saverestricted", session_string=SESSION, api_hash=API_HASH, api_id=API_ID) 
 
 try:
     userbot.start()
 except BaseException:
-    print("Userbot Error! Have you added SESSION while deploying??")
+    print("Userbot Error ! Have you added SESSION while deploying??")
     sys.exit(1)
 
 Bot = Client(
@@ -29,7 +34,7 @@ Bot = Client(
     bot_token=BOT_TOKEN,
     api_id=int(API_ID),
     api_hash=API_HASH
-)
+)    
 
 try:
     Bot.start()
